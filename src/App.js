48cddafb05397e2bf5aed6c7d155828fc710dc0b
Gonza <gonl@gej.com>
2023-07-12 20:28:20 +0200

    feat: Crea nuevo componente y asigna funcion contador.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
+import Boton from './componentes/Boton';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
+
+  const manejarClic = () => {
+    console.log("Clic");
+
+  }
+
+  const reiniciarContador = () => {
+    console.log("reiniciar");
+
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
         <img
-        className='freecodecamp-logo'
-        src={freeCodeCampLogo}
-        alt='Logo de freeCodeCamp'/>
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
-        
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
 
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
       </div>
 
-      
+
     </div>
   );
 }
---
+++ src/componentes/Contador.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contador({ numClics }) {
+    <div className="contador">
+        {numClics}
+    </div>
+}
